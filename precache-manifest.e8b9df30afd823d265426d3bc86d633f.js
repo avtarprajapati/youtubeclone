11fa2ae429e336fe2875fadf0b4b77551bac40ab
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-03-24 19:27:41 +0530

    Updates

---
+++ precache-manifest.e8b9df30afd823d265426d3bc86d633f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2146a5cff5874727627927f13e09e1a3",
+    "url": "/youtubeclone/index.html"
+  },
+  {
+    "revision": "feecad1cf919d4f61e49",
+    "url": "/youtubeclone/static/css/main.a4ec6546.chunk.css"
+  },
+  {
+    "revision": "6b93451de9d983276411",
+    "url": "/youtubeclone/static/js/2.8db87d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtubeclone/static/js/2.8db87d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feecad1cf919d4f61e49",
+    "url": "/youtubeclone/static/js/main.511eb98d.chunk.js"
+  },
+  {
+    "revision": "6f56862df6fa02d09f7e",
+    "url": "/youtubeclone/static/js/runtime-main.d1562302.js"
+  }
+]);
