66c21371749d7b10e3c85f6ddf139d7f6c2a6a4a
unknown <prajapatiavtar@gmail.com>
2020-08-28 11:33:08 +0530

    unused code remove

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,9 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3'
-  // params: {
-  //   maxResults: 5,
-  //   key: 'AIzaSyBFr4ktwpJY002onP9We0VT3xioSud-RTE'
-  // }
+  baseURL: "https://www.googleapis.com/youtube/v3"
 });
